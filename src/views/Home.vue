b7b29a5998c2c6d713cfb77ba3c6b680d0afcbbe
mahatmaArrayyan <muh.mahatma@gmail.com>
2024-09-14 22:01:40 +0800

    navbar update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,26 +13,12 @@ export default {
     name: 'Home',
     data() {
         return {
-            images: null,
-            responsiveOptions: [
-                {
-                    breakpoint: '991px',
-                    numVisible: 4
-                },
-                {
-                    breakpoint: '767px',
-                    numVisible: 3
-                },
-                {
-                    breakpoint: '575px',
-                    numVisible: 1
-                }
-            ]
+            events  : [],
+            news    : [],
+            loker   : []
         }
     },
-    mounted() {
-        PhotoService.getImages().then((data) => (this.images = data));
-    },
+
 }
 </script>
 
