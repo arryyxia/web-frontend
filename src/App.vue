ac3bd32f127233b065364fe36d0aad51bf6f20a3
mahatmaArrayyan <muh.mahatma@gmail.com>
2024-10-02 13:41:20 +0800

    coba dari metatags.io

--- src/App.vue
+++ src/App.vue
@@ -34,30 +34,34 @@ export default {
             pageMeta: {
                 title: 'ANTEK HUB test',
                 meta: [
-                    { name: 'author', content: 'IKATEK UNHAS' },
-                    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                    { name: 'description', content: 'Official Website ANTEK HUB' },
-                    { name: 'keywords', content: 'alumni, alumni teknik unhas, teknik unhas, ikatan alumni unhas' },
-                    
-                    // Open Graph meta tags
-                    { property: 'og:site_name',     content: 'ANTEK HUB' },
-                    { property: 'og:title',         content: 'ANTEK HUB' },
-                    { property: 'og:url',           content: 'https://antekhub.vercel.app/berita' },
-                    { property: 'og:type',          content: 'article' },
-                    { property: 'og:description',   content: 'Official Website ANTEK HUB' },
-                    { property: 'og:locale',        content: 'id_ID' },
+                    { name: 'author',       content: 'IKATEK UNHAS' },
+                    { name: 'viewport',     content: 'width=device-width, initial-scale=1' },
+                    { name: 'description',  content: 'ANTEK HUB Adaptive Colaborative' },
+                    { name: 'title',        content: 'ANTEKHUB - Official Website' },
+                    { name: 'keywords',     content: 'alumni, alumni teknik unhas, teknik unhas, ikatan alumni unhas' },
 
-                    // IMG
-                    { property: 'og:image',     content: 'https://api.antekhub.com/storage/logo-ikatek.PNG' },
-                    { property: 'og:image:url', content: 'https://api.antekhub.com/storage/logo-ikatek.PNG' },
-                    { 
-                        property: 'og:image:secure_url', 
-                        content: 'https://api.antekhub.com/storage/logo-ikatek.PNG'
-                    },
-                    { property: 'og:image:type',    content: 'image/jpeg' },
-                    { property: 'og:image:width',   content: '1200' },
-                    { property: 'og:image:height',  content: '600' },
-                    { property: 'og:image:alt',     content: 'Gambar Preview ANTEK HUB' },  
+                    // Open Graph meta tags
+                    { property: 'og:site_name', content: 'ANTEK HUB' },
+                    { property: 'og:title', content: 'ANTEKHUB - Official Website' },
+                    { property: 'og:url', content: 'https://antekhub.vercel.app/' },
+                    { property: 'og:type', content: 'website' },
+                    { property: 'og:description', content: 'ANTEK HUB Adaptive Colaborative' },
+
+                    // IMG Open Graph
+                    { property: 'og:image', content: 'https://metatags.io/images/meta-tags.png' },
+                    { property: 'og:image:url', content: 'https://metatags.io/images/meta-tags.png' },
+                    { property: 'og:image:secure_url', content: 'https://metatags.io/images/meta-tags.png' },
+                    { property: 'og:image:type', content: 'image/jpeg' },
+                    { property: 'og:image:width', content: '1200' },
+                    { property: 'og:image:height', content: '600' },
+                    { property: 'og:image:alt', content: 'Gambar Preview ANTEK HUB' },
+
+                    // Twitter meta tags
+                    { property: 'twitter:card', content: 'summary_large_image' },
+                    { property: 'twitter:url', content: 'https://antekhub.vercel.app/' },
+                    { property: 'twitter:title', content: 'ANTEKHUB - Official Website' },
+                    { property: 'twitter:description', content: 'ANTEK HUB Adaptive Colaborative' },
+                    { property: 'twitter:image', content: 'https://metatags.io/images/meta-tags.png' },
                 ],
             }
         };
